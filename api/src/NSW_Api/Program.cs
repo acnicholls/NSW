49003dc353f41e8ef9a35b46e94a7f075fdee574
anthony <ac.nicholls@gmail.com>
2023-11-02 19:33:38 -0400

    update API for usage in docker, update solution to hold all projects

--- api/src/NSW_Api/Program.cs
+++ api/src/NSW_Api/Program.cs
@@ -1,11 +1,33 @@
+
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // load the configuration
 builder.Configuration.SetBasePath(Directory.GetCurrentDirectory());
 builder.Configuration.AddUserSecrets("d7df2e78-b68f-405a-821c-48eac048a5a8", true);
+builder.Configuration.AddEnvironmentVariables("NSW_");
 
-builder.Services.AddHttpContextAccessor();
+// configure application 
+builder = NSW.Api.HostExtensions.ConfigureNswKestrel(builder);
+
+// add authentication
+builder.Services.AddAuthentication("Bearer")
+	.AddJwtBearer("Bearer", options =>
+	{
+		options.Authority = "https://localhost";
+		options.MetadataAddress = "http://idp:5006/.well-known/openid-configuration";
+		options.RequireHttpsMetadata = false;
+
+		options.TokenValidationParameters = new TokenValidationParameters
+		{
+			ValidateAudience = false  // for development
+		};
+	});
+
+
 // Add services to the container.
+builder.Services.AddHttpContextAccessor();
 NSW.Info.Extensions.DependencyInjection.RegisterServices(builder.Services);
 NSW.Data.Extensions.DependencyInjecction.RegisterServices(builder.Services);
 NSW.Repositories.Extensions.DependencyInjection.RegisterServices(builder.Services);
@@ -23,15 +45,18 @@ builder.Services.AddSwaggerGen((options) =>
 
 var app = builder.Build();
 
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+	app.UseDeveloperExceptionPage();
 	app.UseSwagger();
 	app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
