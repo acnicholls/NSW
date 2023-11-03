a7e26af6ae236ade8643cfa7c46609ae7f2f0de6
anthony <ac.nicholls@gmail.com>
2023-11-02 23:01:21 -0400

    update geocoder program

--- .tools/geo_coder/NSW_GeoCoder/Program.cs
+++ .tools/geo_coder/NSW_GeoCoder/Program.cs
@@ -1,9 +1,6 @@
-﻿using Microsoft.Data.SqlClient;
-using System.Data;
-using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using NSW.Info.Interfaces;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using NSW.GeoCoder.Interfaces;
 
 var builder = Host.CreateApplicationBuilder(args);
@@ -13,11 +10,10 @@ builder.Configuration.AddJsonFile("appsettings.json", false, true);
 builder.Configuration.AddUserSecrets("d7df2e78-b68f-405a-821c-48eac048a5a8");
 
 NSW.Info.Extensions.DependencyInjection.RegisterServices(builder.Services);
+
 NSW.GeoCoder.Extensions.DependencyInjection.RegisterServices(builder.Services);
 
 var host = builder.Build();
-
-
 
 var serviceContainer = host.Services;
 
@@ -28,14 +24,5 @@ database.AddNewPostalCodes();
 database.ModifyTblUsersPostalCodes();
 database.ReAddFKConstraintOnTblUsers();
 
-
-
-
-
-
-
-
-
-
-
+await host.RunAsync();
 
