ae43c4cc511d887cb0e6fc8fbb17499869b17662
anthony <ac.nicholls@gmail.com>
2023-11-06 21:43:28 -0500

    clean up program

--- bff/src/Program.cs
+++ bff/src/Program.cs
@@ -1,19 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Events;
+using System;
 
 namespace Starter.Bff
 {
-    public class Program
+	public class Program
     {
         public static int Main(string[] args)
         {
