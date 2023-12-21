a874ec2d913cb00be6697ddb7e0363b2ce8bd30d
anthony <ac.nicholls@gmail.com>
2023-12-20 22:37:57 -0500

    remove weather portion

--- app/cra/src/components/About.jsx
+++ app/cra/src/components/About.jsx
@@ -1,30 +1,13 @@
-import React, { useState, useEffect } from "react";
-import { useWeatherContext } from "../contexts/WeatherContext";
+import React from "react";
 import { Row, Col } from "react-bootstrap";
 
 const About = () => {
-  const { weather, getWeather } = useWeatherContext();
-
-  getWeather();
-
-  let displaySection = <></>;
-  if (weather) {
-    displaySection = (
-      <>
-        <div>
-          <code>{JSON.stringify(weather)}</code>
-        </div>
-      </>
-    );
-  }
-
   return (
     <>
       <Row>
         <Col></Col>
         <Col>
           <div>{"This is the about page."}</div>
-          {displaySection}
         </Col>
         <Col></Col>
       </Row>
