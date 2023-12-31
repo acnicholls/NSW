52dea3ddc167a9306d2a653f8dc396d299e2f36f
anthony <ac.nicholls@gmail.com>
2023-12-30 22:56:26 -0500

    update title bar component

--- app/cra/src/components/TitleBarComponent.jsx
+++ app/cra/src/components/TitleBarComponent.jsx
@@ -1,15 +1,13 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 
 const TitleBarComponent = () => {
   return (
     <>
       <Row>
-        <Col></Col>
         <Col>
           <h1>Natsuko's Sharing Website</h1>
         </Col>
-        <Col></Col>
       </Row>
     </>
   );
