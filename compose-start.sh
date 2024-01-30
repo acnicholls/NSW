82fca2e651fe8a8491e344dff3c71f740a0648c4
anthony <ac.nicholls@gmail.com>
2024-01-29 19:33:53 -0500

    final changes

--- compose-start.sh
+++ compose-start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# this file is where a developer will start
+# this file is where a developer will start.
 # this will bring up each part of the application
-# as it's own container in a compose solution
-# all setup using mounts so that changes to the 
-# host file system are caught and the applications
+# as it's own service in a compose solution
+# all set up using mounts so that changes to the 
+# host file system are caught and the application's
 # parts are re-loaded, giving the dev real-time
 # change detection
 
